e44d714da6541ecbd637f13e730e28ba147949e0
huangsongyao <huangsongyao@foxmail.com>
2018-04-13 16:57:28 +0800

    添加方法

--- HSYCocoaKit/Classes/ProjectManager/HSYCocoaKitManager.h
+++ HSYCocoaKit/Classes/ProjectManager/HSYCocoaKitManager.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "JSONModel.h"
 
 @interface HSYCocoaKitManager : NSObject
 
@@ -18,7 +19,7 @@
  @param key key
  @return id
  */
-+ (id)objectForKey:(NSString *)key;
++ (id)hsy_objectForKey:(NSString *)key;
 
 /**
  NSUserDefaults设置缓存内容
@@ -26,14 +27,78 @@
  @param object 缓存对象
  @param key key
  */
-+ (void)setObject:(id)object forKey:(NSString *)key;
++ (void)hsy_setObject:(id)object forKey:(NSString *)key;
 
 /**
  NSUserDefaults删除缓存内容
 
  @param key key
  */
-+ (void)removeObjectForKey:(NSString *)key;
++ (void)hsy_removeObjectForKey:(NSString *)key;
 
+/**
+ NSUserDefaults获取JSONModel对象
+
+ @param key key
+ @param classes JSONModel子类的类型
+ @return JSONModel
+ */
++ (JSONModel *)hsy_JSONModelForKey:(NSString *)key class:(Class)classes;
+
+/**
+ NSUserDefaults缓存SONModel对象
+
+ @param model model
+ @param key key
+ */
++ (void)hsy_setJSNModel:(JSONModel *)model forKey:(NSString *)key;
+
+/**
+ NSUserDefaults获取字典
+
+ @param key key
+ @return NSDictionary
+ */
++ (NSDictionary *)hsy_dictionaryForKey:(NSString *)key;
+
+/**
+ NSUserDefaults缓存字典
+
+ @param dic 字典
+ @param key key
+ */
++ (void)hsy_setDictionary:(NSDictionary *)dic forKey:(NSString *)key;
+
+/**
+ NSUserDefaults获取数组
+
+ @param key key
+ @return NSArray
+ */
++ (NSArray *)hsy_arrayForKey:(NSString *)key;
+
+/**
+ NSUserDefaults缓存数组
+
+ @param array 数组
+ @param key key
+ */
++ (void)hsy_setArray:(NSArray *)array forKey:(NSString *)key;
+
+/**
+ NSUserDefaults获取字符串
+
+ @param key key
+ @return NSString
+ */
++ (NSString *)hsy_stringForKey:(NSString *)key;
+
+/**
+ NSUserDefaults缓存字符串
+
+ @param string 字符串
+ @param key key
+ */
++ (void)hsy_setString:(NSString *)string forKey:(NSString *)key;
 
 @end
--- HSYCocoaKit/Classes/ProjectManager/HSYCocoaKitManager.m
+++ HSYCocoaKit/Classes/ProjectManager/HSYCocoaKitManager.m
@@ -31,25 +31,73 @@ static HSYCocoaKitManager *cocoaKitManager;
 
 #pragma mark - UserDefaults
 
-+ (id)objectForKey:(NSString *)key
++ (id)hsy_objectForKey:(NSString *)key
 {
     NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
     id object = [userDefaults objectForKey:key];
     return object;
 }
 
-+ (void)setObject:(id)object forKey:(NSString *)key
++ (void)hsy_setObject:(id)object forKey:(NSString *)key
 {
-    [self.class removeObjectForKey:key];
+    [self.class hsy_removeObjectForKey:key];
     NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
     [userDefaults setObject:object forKey:key];
 }
 
-+ (void)removeObjectForKey:(NSString *)key
++ (void)hsy_removeObjectForKey:(NSString *)key
 {
     NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
     [userDefaults removeObjectForKey:key];
 }
 
++ (JSONModel *)hsy_JSONModelForKey:(NSString *)key class:(__unsafe_unretained Class)classes
+{
+    id object = [self.class hsy_objectForKey:key];
+    JSONModel *model = [NSObject hsy_resultObjectToJSONModelWithClasses:classes json:object];
+    return model;
+}
+
++ (void)hsy_setJSNModel:(JSONModel *)model forKey:(NSString *)key
+{
+    NSDictionary *json = model.toDictionary;
+    [self.class hsy_setObject:json forKey:key];
+}
+
++ (NSDictionary *)hsy_dictionaryForKey:(NSString *)key
+{
+    id object = [self.class hsy_objectForKey:key];
+    NSDictionary *dic = (NSDictionary *)object;
+    return dic;
+}
+
++ (void)hsy_setDictionary:(NSDictionary *)dic forKey:(NSString *)key
+{
+    [self.class hsy_setObject:dic forKey:key];
+}
+
++ (NSArray *)hsy_arrayForKey:(NSString *)key
+{
+    id object = [self.class hsy_objectForKey:key];
+    NSArray *array = (NSArray *)object;
+    return array;
+}
+
++ (void)hsy_setArray:(NSArray *)array forKey:(NSString *)key
+{
+    [self.class hsy_setObject:array forKey:key];
+}
+
++ (NSString *)hsy_stringForKey:(NSString *)key
+{
+    id object = [self.class hsy_objectForKey:key];
+    NSString *string = (NSString *)object;
+    return string;
+}
+
++ (void)hsy_setString:(NSString *)string forKey:(NSString *)key
+{
+    [self.class hsy_setObject:string forKey:key];
+}
 
 @end
