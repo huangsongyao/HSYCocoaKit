7b0aefab21595701fb039ab0b6af1dd30e1e0a9d
huangsongyao <huangsongyao@foxmail.com>
2018-04-13 14:44:00 +0800

    添加方法

--- HSYCocoaKit/Classes/ProjectManager/HSYCocoaKitManager.m
+++ HSYCocoaKit/Classes/ProjectManager/HSYCocoaKitManager.m
@@ -31,9 +31,25 @@ static HSYCocoaKitManager *cocoaKitManager;
 
 #pragma mark - UserDefaults
 
++ (id)objectForKey:(NSString *)key
+{
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    id object = [userDefaults objectForKey:key];
+    return object;
+}
+
 + (void)setObject:(id)object forKey:(NSString *)key
 {
-    
+    [self.class removeObjectForKey:key];
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:object forKey:key];
 }
+
++ (void)removeObjectForKey:(NSString *)key
+{
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults removeObjectForKey:key];
+}
+
 
 @end
