f3ac588143a1e1663253b162a2c78dcd371b4112
huangsongyao <huangsongyao@foxmail.com>
2018-05-17 14:35:07 +0800

    判断方法调整

--- HSYCocoaKit/Classes/Category/UIApplication+Device.m
+++ HSYCocoaKit/Classes/Category/UIApplication+Device.m
@@ -8,6 +8,8 @@
 
 #import "UIApplication+Device.h"
 #import "NSFileManager+Finder.h"
+#import "PublicMacroFile.h"
+#import "HSYBaseLaunchScreenViewController.h"
 
 @implementation UIApplication (Device)
 
@@ -28,7 +30,11 @@
 
 + (CGFloat)statusBarHeight
 {
-    return [UIApplication iPhoneStatusBarSize].height;
+    CGFloat height = [UIApplication iPhoneStatusBarSize].height;
+    if (height == 0.0f) {
+        height = (IPHONE_HEIGHT == ((CGFloat)kHSYCocoaKitLaunchScreenSize_5_8_Inch) ? 44.0f : 20.0f);
+    }
+    return height;
 }
 
 + (id<UIApplicationDelegate>)appDelegate
