79b4acac831a2fcc6ae4505549e82144285ecc19
huangsongyao <huangsongyao@foxmail.com>
2018-06-21 13:52:10 +0800

    添加手势

--- HSYCocoaKit/Classes/Category/UIView+Gestures.h
+++ HSYCocoaKit/Classes/Category/UIView+Gestures.h
@@ -10,6 +10,8 @@
 
 @interface UIView (Gestures)
 
+#pragma mark - Tap
+
 /**
  添加单指单击手势，手指数1，单击次数1
 
@@ -26,12 +28,45 @@
  */
 - (void)hsy_addDoubleGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate subscribeNext:(void(^)(UITapGestureRecognizer *gesture))next;
 
+#pragma mark - Longs
+
 /**
- 添加長按手勢，響應時間為0.5f秒
+ 添加長按手勢
  
  @param delegate 委託
+ @param duration 响应时间
  @param next 長按響應事件
  */
-- (void)hsy_addLongGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate subscribeNext:(void(^)(UILongPressGestureRecognizer *gesture))next;
+- (void)hsy_addLongGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate minimumPressDuration:(CFTimeInterval)duration subscribeNext:(void(^)(UILongPressGestureRecognizer *gesture))next;
+
+#pragma mark - Swipe
+
+/**
+ 添加单方向滑动手势
+
+ @param delegate 委托
+ @param direction 滑动方向枚举
+ @param next 滑动响应事件
+ */
+- (void)hsy_addSwipeGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate recognizerDirection:(UISwipeGestureRecognizerDirection)direction subscribeNext:(void(^)(UISwipeGestureRecognizer *gesture))next;
+
+/**
+ 添加多个方向的滑动手势
+
+ @param delegate 委托
+ @param directions 方向枚举集合
+ @param next 滑动响应事件
+ */
+- (void)hsy_addSwipeGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate recognizerDirections:(NSArray<NSNumber *> *)directions subscribeNext:(void(^)(UISwipeGestureRecognizer *gesture, UISwipeGestureRecognizerDirection direction))next;
+
+#pragma mark - Pan
+
+/**
+ 添加上下左右四个方向的滑动手势
+
+ @param delegate 委托
+ @param next 滑动响应事件
+ */
+- (void)hsy_addPanGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate subscribeNext:(void(^)(UIPanGestureRecognizer *gesture))next;
 
 @end
--- HSYCocoaKit/Classes/Category/UIView+Gestures.m
+++ HSYCocoaKit/Classes/Category/UIView+Gestures.m
@@ -60,11 +60,63 @@
     return longs;
 }
 
-- (void)hsy_addLongGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate subscribeNext:(void(^)(UILongPressGestureRecognizer *gesture))next
+- (void)hsy_addLongGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate minimumPressDuration:(CFTimeInterval)duration subscribeNext:(void(^)(UILongPressGestureRecognizer *gesture))next
 {
-    UILongPressGestureRecognizer *longs = [self.class hsy_addLongGestureRecognizerDelegate:delegate minimumPressDuration:0.5f subscribeNext:next];
+    UILongPressGestureRecognizer *longs = [self.class hsy_addLongGestureRecognizerDelegate:delegate minimumPressDuration:duration subscribeNext:next];
     [self addGestureRecognizer:longs];
 }
 
+#pragma mark - Swipe
+
++ (UISwipeGestureRecognizer *)hsy_addSwipeGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate recognizerDirection:(UISwipeGestureRecognizerDirection)direction subscribeNext:(void(^)(UISwipeGestureRecognizer *gesture))next
+{
+    UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] init];
+    swipe.direction = direction;
+    swipe.delegate = delegate;
+    [[[swipe rac_gestureSignal] deliverOn:[RACScheduler mainThreadScheduler]] subscribeNext:^(id x) {
+        if (next) {
+            next(x);
+        }
+    }];
+    return swipe;
+}
+
+- (void)hsy_addSwipeGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate recognizerDirection:(UISwipeGestureRecognizerDirection)direction subscribeNext:(void(^)(UISwipeGestureRecognizer *gesture))next
+{
+    UISwipeGestureRecognizer *swipe = [self.class hsy_addSwipeGestureRecognizerDelegate:delegate recognizerDirection:direction subscribeNext:next];
+    [self addGestureRecognizer:swipe];
+}
+
+- (void)hsy_addSwipeGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate recognizerDirections:(NSArray<NSNumber *> *)directions subscribeNext:(void(^)(UISwipeGestureRecognizer *gesture, UISwipeGestureRecognizerDirection direction))next
+{
+    for (NSNumber *number in directions) {
+        UISwipeGestureRecognizerDirection direction = (UISwipeGestureRecognizerDirection)number.integerValue;
+        [self hsy_addSwipeGestureRecognizerDelegate:delegate recognizerDirection:direction subscribeNext:^(UISwipeGestureRecognizer *gesture) {
+            if (next) {
+                next(gesture, gesture.direction);
+            }
+        }];
+    }
+}
+
+#pragma mark - Pan
+
++ (UIPanGestureRecognizer *)hsy_addPanGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate subscribeNext:(void(^)(UIPanGestureRecognizer *gesture))next
+{
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] init];
+    pan.delegate = delegate;
+    [[[pan rac_gestureSignal] deliverOn:[RACScheduler mainThreadScheduler]] subscribeNext:^(id x) {
+        if (next) {
+            next(x);
+        }
+    }];
+    return pan;
+}
+
+- (void)hsy_addPanGestureRecognizerDelegate:(id<UIGestureRecognizerDelegate>)delegate subscribeNext:(void(^)(UIPanGestureRecognizer *gesture))next
+{
+    UIPanGestureRecognizer *pan = [self.class hsy_addPanGestureRecognizerDelegate:delegate subscribeNext:next];
+    [self addGestureRecognizer:pan];
+}
 
 @end
