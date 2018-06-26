08baab7388978491b9a9a9c76fafd02f1c1d0ecd
huangsongyao <huangsongyao@foxmail.com>
2018-06-26 16:30:03 +0800

    添加willDealloc的rac信号log打印

--- HSYCocoaKit/Classes/BaseModel/HSYBaseModel.m
+++ HSYCocoaKit/Classes/BaseModel/HSYBaseModel.m
@@ -21,6 +21,11 @@
     if (self = [super init]) {
         self.hsy_datas = [[NSMutableArray alloc] init];
         _subject = [RACSubject subject];
+        @weakify(self);
+        [[self.subject rac_willDeallocSignal] subscribeCompleted:^{
+            @strongify(self);
+            NSLog(@"hot signal---subject release: class is %@", NSStringFromClass(self.class));
+        }];
     }
     return self;
 }
