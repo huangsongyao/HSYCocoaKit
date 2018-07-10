879cde199d8935c639d1d3233197669626ed05f7
huangsongyao <huangsongyao@foxmail.com>
2018-07-10 19:30:48 +0800

    添加单个类型的接口方法

--- HSYCocoaKit/Classes/CustomNavigationController/BaseCustomNavigationController/HSYBaseViewController+CustomNavigationItem.h
+++ HSYCocoaKit/Classes/CustomNavigationController/BaseCustomNavigationController/HSYBaseViewController+CustomNavigationItem.h
@@ -10,60 +10,119 @@
 
 @interface HSYBaseViewController (CustomNavigationItem)
 
+#pragma mark - Left---Double
+
 /**
  快速创建左部分的导航栏按钮----图片类型
  
- @param images 图片内容，格式为：@{@(导航栏按钮tag) : @"图片名称")}
+ @param images 图片内容，格式为：@[@{@(导航栏按钮tag) : @"图片名称")}, ...]
  @param next 按钮点击事件
  */
 - (void)hsy_leftItemsImages:(NSArray<NSDictionary *> *)images
               subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
 
 /**
- 快速创建右部分的导航栏按钮----图片类型
- 
- @param images 图片内容，格式为@{@(导航栏按钮tag) : @"图片名称")}
- @param next 按钮点击事件
- */
-- (void)hsy_rightItemsImages:(NSArray<NSDictionary *> *)images
-               subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
-
-/**
  快速创建左部分的导航栏按钮----文字类型，默认黑色字体颜色，15号字号
  
- @param titles title内容，格式为：@{@(导航栏按钮tag) : @"title内容"}
+ @param titles title内容，格式为：@[@{@(导航栏按钮tag) : @"title内容"}, ...]
  @param next 按钮点击事件
  */
 - (void)hsy_leftItemsTitles:(NSArray<NSDictionary *> *)titles
               subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
 
 /**
+ 快速创建左部分的导航栏按钮----文字类型
+ 
+ @param titles title内容，格式为：@[@{@(导航栏按钮tag) : @"title内容"}, ...]
+ @param titleColors 字体颜色集合
+ @param titleFonts 字体字号集合
+ @param next 按钮点击事件
+ */
+- (void)hsy_leftItemsTitles:(NSArray<NSDictionary *> *)titles
+                titleColors:(NSArray<UIColor *> *)titleColors
+                 titleFonts:(NSArray<UIFont *> *)titleFonts
+              subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
+
+#pragma mark - Right---Double
+
+/**
+ 快速创建右部分的导航栏按钮----图片类型
+ 
+ @param images 图片内容，格式为：@[@{@(导航栏按钮tag) : @"图片名称")}, ...]
+ @param next 按钮点击事件
+ */
+- (void)hsy_rightItemsImages:(NSArray<NSDictionary *> *)images
+               subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
+
+/**
  快速创建右部分的导航栏按钮----文字类型，默认黑色字体颜色，15号字号
  
- @param titles title内容，格式为：@{@(导航栏按钮tag) : @"title内容"}
+ @param titles title内容，格式为：@[@{@(导航栏按钮tag) : @"title内容"}, ...]
  @param next 按钮点击事件
  */
 - (void)hsy_rightItemsTitles:(NSArray<NSDictionary *> *)titles
                subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
 
 /**
- 快速创建左部分的导航栏按钮----文字类型
-
- @param titles title内容，格式为：@{@(导航栏按钮tag) : @"title内容"}
- @param titleColors 字体颜色
- @param titleFonts 字体字号
- @param next 按钮点击事件
- */
-- (void)hsy_leftItemsTitles:(NSArray<NSDictionary *> *)titles titleColors:(NSArray<UIColor *> *)titleColors titleFonts:(NSArray<UIFont *> *)titleFonts subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
-
-/**
  快速创建右部分的导航栏按钮----文字类型
 
- @param titles title内容，格式为：@{@(导航栏按钮tag) : @"title内容"}
- @param titleColors 字体颜色
- @param titleFonts 字体字号
+ @param titles title内容，格式为：@[@{@(导航栏按钮tag) : @"title内容"}, ...]
+ @param titleColors 字体颜色集合
+ @param titleFonts 字体字号集合
  @param next 按钮点击事件
  */
-- (void)hsy_rightItemsTitles:(NSArray<NSDictionary *> *)titles titleColors:(NSArray<UIColor *> *)titleColors titleFonts:(NSArray<UIFont *> *)titleFonts subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
+- (void)hsy_rightItemsTitles:(NSArray<NSDictionary *> *)titles
+                 titleColors:(NSArray<UIColor *> *)titleColors
+                  titleFonts:(NSArray<UIFont *> *)titleFonts
+               subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
+
+#pragma mark - Left---Single
+
+/**
+ 快速创建左部分的导航栏按钮----文字类型，单个类型
+
+ @param titleParamter 格式为：@{@(导航栏按钮tag) : @"title内容"}
+ @param titleColor 字体颜色
+ @param titleFont 字体字号
+ @param next 点击回调事件
+ */
+- (void)hsy_leftItemTitleParamter:(NSDictionary *)titleParamter
+                       titleColor:(UIColor *)titleColor
+                        titleFont:(UIFont *)titleFont
+                    subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
+
+/**
+ 快速创建左部分的导航栏按钮----图片类型，单个类型
+
+ @param imageParamter 格式为：@{@(导航栏按钮tag) : @"图片名称")}
+ @param next 点击回调事件
+ */
+- (void)hsy_leftItemImageParamter:(NSDictionary *)imageParamter
+                    subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
+
+#pragma mark - Right---Single
+
+/**
+ 快速创建右部分的导航栏按钮----文字类型，单个类型
+
+ @param titleParamter 格式为：@{@(导航栏按钮tag) : @"title内容"}
+ @param titleColor 字体颜色
+ @param titleFont 字体字号
+ @param next 点击回调事件
+ */
+- (void)hsy_rightItemTitleParamter:(NSDictionary *)titleParamter
+                        titleColor:(UIColor *)titleColor
+                         titleFont:(UIFont *)titleFont
+                     subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
+
+/**
+ 快速创建右部分的导航栏按钮----图片类型，单个类型
+
+ @param imageParamter 格式为：@{@(导航栏按钮tag) : @"图片名称")}
+ @param next 点击回调事件
+ */
+- (void)hsy_rightItemImageParamter:(NSDictionary *)imageParamter
+                     subscribeNext:(void(^)(UIButton *button, NSInteger tag))next;
+
 
 @end
--- HSYCocoaKit/Classes/CustomNavigationController/BaseCustomNavigationController/HSYBaseViewController+CustomNavigationItem.m
+++ HSYCocoaKit/Classes/CustomNavigationController/BaseCustomNavigationController/HSYBaseViewController+CustomNavigationItem.m
@@ -10,24 +10,11 @@
 
 @implementation HSYBaseViewController (CustomNavigationItem)
 
+#pragma mark - Left Double BarButton
+
 - (void)hsy_leftItemsImages:(NSArray<NSDictionary *> *)images subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
 {
     self.hsy_customNavigationBarNavigationItem.leftBarButtonItems = [self.class hsy_barButtonItemsImages:images edgeInsetsLeft:-DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
-}
-
-- (void)hsy_rightItemsImages:(NSArray<NSDictionary *> *)images subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
-{
-    self.hsy_customNavigationBarNavigationItem.rightBarButtonItems = [self.class hsy_barButtonItemsImages:images edgeInsetsLeft:DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
-}
-
-- (void)hsy_leftItemsTitles:(NSArray<NSDictionary *> *)titles subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
-{
-    self.hsy_customNavigationBarNavigationItem.leftBarButtonItems = [self.class hsy_barButtonItemsTitles:titles edgeInsetsLeft:-DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
-}
-
-- (void)hsy_rightItemsTitles:(NSArray<NSDictionary *> *)titles subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
-{
-    self.hsy_customNavigationBarNavigationItem.rightBarButtonItems = [self.class hsy_barButtonItemsTitles:titles edgeInsetsLeft:DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
 }
 
 - (void)hsy_leftItemsTitles:(NSArray<NSDictionary *> *)titles titleColors:(NSArray<UIColor *> *)titleColors titleFonts:(NSArray<UIFont *> *)titleFonts subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
@@ -35,9 +22,50 @@
     self.hsy_customNavigationBarNavigationItem.leftBarButtonItems = [self.class hsy_barButtonItemsTitles:titles titleColors:titleColors titleFonts:titleFonts edgeInsetsLeft:-DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
 }
 
+- (void)hsy_leftItemsTitles:(NSArray<NSDictionary *> *)titles subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
+{
+    self.hsy_customNavigationBarNavigationItem.leftBarButtonItems = [self.class hsy_barButtonItemsTitles:titles edgeInsetsLeft:-DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
+}
+
+#pragma mark - Right Double BarButton
+
+- (void)hsy_rightItemsImages:(NSArray<NSDictionary *> *)images subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
+{
+    self.hsy_customNavigationBarNavigationItem.rightBarButtonItems = [self.class hsy_barButtonItemsImages:images edgeInsetsLeft:DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
+}
+
+- (void)hsy_rightItemsTitles:(NSArray<NSDictionary *> *)titles subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
+{
+    self.hsy_customNavigationBarNavigationItem.rightBarButtonItems = [self.class hsy_barButtonItemsTitles:titles edgeInsetsLeft:DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
+}
+
 - (void)hsy_rightItemsTitles:(NSArray<NSDictionary *> *)titles titleColors:(NSArray<UIColor *> *)titleColors titleFonts:(NSArray<UIFont *> *)titleFonts subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
 {
     self.hsy_customNavigationBarNavigationItem.rightBarButtonItems = [self.class hsy_barButtonItemsTitles:titles titleColors:titleColors titleFonts:titleFonts edgeInsetsLeft:DEFAULT_BUTTOM_EDGE_INSETS_LEFT subscribeNext:next];
+}
+
+#pragma mark - Left Single BarButton
+
+- (void)hsy_leftItemTitleParamter:(NSDictionary *)titleParamter titleColorParamter:(UIColor *)titleColorParamter titleFontParamter:(UIFont *)titleFontParamter subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
+{
+    [self hsy_leftItemsTitles:@[titleParamter] titleColors:@[titleColorParamter] titleFonts:@[titleFontParamter] subscribeNext:next];
+}
+
+- (void)hsy_leftItemImageParamter:(NSDictionary *)imageParamter subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
+{
+    [self hsy_leftItemsImages:@[imageParamter] subscribeNext:next];
+}
+
+#pragma mark - Right Single BarButton
+
+- (void)hsy_rightItemTitleParamter:(NSDictionary *)titleParamter titleColorParamter:(UIColor *)titleColorParamter titleFontParamter:(UIFont *)titleFontParamter subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
+{
+    [self hsy_rightItemsTitles:@[titleParamter] titleColors:@[titleColorParamter] titleFonts:@[titleFontParamter] subscribeNext:next];
+}
+
+- (void)hsy_rightItemImageParamter:(NSDictionary *)imageParamter subscribeNext:(void(^)(UIButton *button, NSInteger tag))next
+{
+    [self hsy_rightItemsImages:@[imageParamter] subscribeNext:next];
 }
 
 @end
