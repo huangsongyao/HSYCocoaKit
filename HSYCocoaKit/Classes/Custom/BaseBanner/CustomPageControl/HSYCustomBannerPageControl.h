d020d796f0b5adb6bb3e50544876a10430690549
huangsongyao <huangsongyao@foxmail.com>
2018-07-12 14:45:01 +0800

    提供可供定制化的UIPageControl

--- HSYCocoaKit/Classes/Custom/BaseBanner/CustomPageControl/HSYCustomBannerPageControl.h
+++ HSYCocoaKit/Classes/Custom/BaseBanner/CustomPageControl/HSYCustomBannerPageControl.h
@@ -7,6 +7,38 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, kHSYCocoaKitCustomBannerPageControl) {
+    
+    kHSYCocoaKitCustomBannerPageControlNumberOfPages                    = 456,  //总数--NSNumber--NSInteger
+    kHSYCocoaKitCustomBannerPageControlCurrentPage,                             //当前页面--NSNumber--NSInteger
+    kHSYCocoaKitCustomBannerPageControlPageIndicatorTintColor,                  //未选中的颜色--UIColor
+    kHSYCocoaKitCustomBannerPageControlCurrentPageIndicatorTintColor,           //选中项的颜色--UIColor
+    
+};
+
 @interface HSYCustomBannerPageControl : UIPageControl
+
+- (instancetype)initWithParamter:(NSDictionary<NSNumber *, id> *)param;
+
+/**
+ 点的高度，允许子类重写本方法，定制高度，默认为3.0f
+ 
+ @return 点的高度
+ */
++ (CGFloat)hsy_defaultPageControlPointHeight;
+
+/**
+ 选中的点的宽度，允许子类重写本方法，定制高度，默认为10.0f
+ 
+ @return 选中的点的宽度
+ */
++ (CGFloat)hsy_defaultPageControlPointSelectedPointWidth;
+
+/**
+ 点与点之间的间距，允许子类重写本方法，定制高度，默认为5.0f
+ 
+ @return 点与点之间的间距
+ */
++ (CGFloat)hsy_defaultPageControlPointMagrin;
 
 @end
--- HSYCocoaKit/Classes/Custom/BaseBanner/CustomPageControl/HSYCustomBannerPageControl.m
+++ HSYCocoaKit/Classes/Custom/BaseBanner/CustomPageControl/HSYCustomBannerPageControl.m
@@ -6,8 +6,72 @@
 //
 
 #import "HSYCustomBannerPageControl.h"
+#import "PublicMacroFile.h"
+#import "UIView+Frame.h"
+
+#define DEFAULT_PAGE_PIONT_HEIGHT           3.0f
+#define DEFAULT_PAGE_PIONT_WIDHT            10.0f
+#define DEFAULT_PAGE_PIONT_MAGRIN           5.0f
 
 @implementation HSYCustomBannerPageControl
+
+- (instancetype)initWithParamter:(NSDictionary<NSNumber *,id> *)param
+{
+    if (self = [super initWithSize:CGSizeMake(IPHONE_WIDTH, DEFAULT_PAGE_PIONT_HEIGHT)]) {
+        if (param[@(kHSYCocoaKitCustomBannerPageControlNumberOfPages)]) {
+            self.numberOfPages = [param[@(kHSYCocoaKitCustomBannerPageControlNumberOfPages)] integerValue];
+        }
+        if (param[@(kHSYCocoaKitCustomBannerPageControlCurrentPage)]) {
+            self.currentPage = [param[@(kHSYCocoaKitCustomBannerPageControlCurrentPage)] integerValue];
+        }
+        if (param[@(kHSYCocoaKitCustomBannerPageControlPageIndicatorTintColor)]) {
+            self.pageIndicatorTintColor = param[@(kHSYCocoaKitCustomBannerPageControlPageIndicatorTintColor)];
+        }
+        if (param[@(kHSYCocoaKitCustomBannerPageControlCurrentPageIndicatorTintColor)]) {
+            self.currentPageIndicatorTintColor = param[@(kHSYCocoaKitCustomBannerPageControlCurrentPageIndicatorTintColor)];
+        }
+    }
+    return self;
+}
+
+#pragma mark - Custom
+
++ (CGFloat)hsy_defaultPageControlPointHeight
+{
+    return DEFAULT_PAGE_PIONT_HEIGHT;
+}
+
++ (CGFloat)hsy_defaultPageControlPointSelectedPointWidth
+{
+    return DEFAULT_PAGE_PIONT_WIDHT;
+}
+
++ (CGFloat)hsy_defaultPageControlPointMagrin
+{
+    return DEFAULT_PAGE_PIONT_MAGRIN;
+}
+
+#pragma mark - Load
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    CGFloat realWidth = (self.subviews.count - 1) * ([self.class hsy_defaultPageControlPointMagrin] + [self.class hsy_defaultPageControlPointHeight]) + [self.class hsy_defaultPageControlPointSelectedPointWidth];
+    self.frame = CGRectMake((IPHONE_WIDTH - realWidth)/2, self.y, realWidth, self.height);
+    self.center = CGPointMake(IPHONE_WIDTH/2, self.center.y);
+    CGFloat x = 0.0f;
+    for (NSUInteger i = 0; i < self.subviews.count; i ++) {
+        UIView *subview = [self.subviews objectAtIndex:i];
+        CGFloat width = [self.class hsy_defaultPageControlPointHeight];
+        if (i == self.currentPage) {
+            width = [self.class hsy_defaultPageControlPointSelectedPointWidth];
+        }
+        subview.frame = CGRectMake(x, subview.frame.origin.y, width, [self.class hsy_defaultPageControlPointHeight]);
+        subview.layer.cornerRadius = CGRectGetHeight(subview.frame)/2;
+        x = (CGRectGetMaxX(subview.frame) + [self.class hsy_defaultPageControlPointMagrin]);
+    }
+}
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
