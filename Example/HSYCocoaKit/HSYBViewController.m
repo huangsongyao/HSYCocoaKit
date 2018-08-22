1f33dd6bdc22c2eefae880b8d1caf4c03c5bb25e
huangsongyao <huangsongyao@foxmail.com>
2018-08-22 15:17:20 +0800

    0.5.2更新节点

--- Example/HSYCocoaKit/HSYBViewController.m
+++ Example/HSYCocoaKit/HSYBViewController.m
@@ -112,25 +112,9 @@
     self.registerClasses = @{@"TestBaseTableViewCell" : @"uuuufffff"};
     [super viewDidLoad];
     
-    
+    self.hsy_showLoading = YES;
 //    self.tableView.hidden = YES;
-    self.tableHeaderView = [self header];
-    self.tableView.tableHeaderView = self.tableHeaderView;
-    
     @weakify(self);
-    UIButton *btn = [NSObject createButtonByParam:@{@(kHSYCocoaKitOfButtonPropretyTypeNorTitle) : @"返回"} clickedOnSubscribeNext:^(UIButton *button) {
-        @strongify(self);
-        [self update:NO];
-    }];
-    btn.frame = CGRectMake(0, 0, 50, 35);
-    self.headerView = [[UIView alloc] initWithSize:CGSizeMake(IPHONE_WIDTH, 35)];
-    self.headerView.y = self.customNavigationBar.bottom;
-    self.headerView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:self.headerView];
-    [self.view bringSubviewToFront:self.headerView];
-    self.headerView.hidden = YES;
-    [self.headerView addSubview:btn];
-    
     [self hsy_rightItemsImages:@[@{@(kHSYCocoaKitDefaultCustomBarItemTag) : @"nav_back@2x"}] subscribeNext:^(UIButton *button, NSInteger tag) {
 //        @strongify(self);
         NSLog(@"x1=%@", [NSDate date]);
@@ -156,29 +140,47 @@
     }];
     
     
-    UIButton *button = [NSObject createButtonByParam:@{} clickedOnSubscribeNext:^(UIButton *button) {
-        NSLog(@"5555555555555555555");
-    }];
-    button.backgroundColor = [UIColor redColor];
-    button.frame = CGRectMake(100, 300, 60, 56);
-    [self.view addSubview:button];
-    
-    
-    NSMutableArray *banners = [[NSMutableArray alloc] init];
-    for (NSInteger i = 0; i < 10; i ++) {
-//        HSYCustomBannerDataSourece *data = [[HSYCustomBannerDataSourece alloc] init];
-        [banners addObject:[NSString stringWithFormat:@"%@", @(i)]];
-    }
-    _testBanners = [banners mutableCopy];
+//    UIButton *button = [NSObject createButtonByParam:@{} clickedOnSubscribeNext:^(UIButton *button) {
+//        NSLog(@"5555555555555555555");
+//    }];
+//    button.backgroundColor = [UIColor redColor];
+//    button.frame = CGRectMake(100, 300, 60, 56);
+//    [self.view addSubview:button];
+//
+//
+//    NSMutableArray *banners = [[NSMutableArray alloc] init];
+//    for (NSInteger i = 0; i < 10; i ++) {
+////        HSYCustomBannerDataSourece *data = [[HSYCustomBannerDataSourece alloc] init];
+//        [banners addObject:[NSString stringWithFormat:@"%@", @(i)]];
+//    }
+//    _testBanners = [banners mutableCopy];
     
     
     [self firstRequest];
     [[self.hsy_viewModel.subject deliverOn:[RACScheduler mainThreadScheduler]] subscribeNext:^(id x) {
-        NSLog(@"");
+        @strongify(self);
+        [self.tableView reloadData];
+        [self hsy_endSystemLoading];
+//        self.tableHeaderView = [self header];
+//        self.tableView.tableHeaderView = self.tableHeaderView;
+//        
+//        
+//        UIButton *btn = [NSObject createButtonByParam:@{@(kHSYCocoaKitOfButtonPropretyTypeNorTitle) : @"返回"} clickedOnSubscribeNext:^(UIButton *button) {
+//            @strongify(self);
+//            [self update:NO];
+//        }];
+//        btn.frame = CGRectMake(0, 0, 50, 35);
+//        self.headerView = [[UIView alloc] initWithSize:CGSizeMake(IPHONE_WIDTH, 35)];
+//        self.headerView.y = self.customNavigationBar.bottom;
+//        self.headerView.backgroundColor = [UIColor redColor];
+//        [self.view addSubview:self.headerView];
+//        [self.view bringSubviewToFront:self.headerView];
+//        self.headerView.hidden = YES;
+//        [self.headerView addSubview:btn];
     }];
     
     self.hsy_viewModel.hsy_showPromptContent = NO;
-    self.pullUpStatus = kHSYCocoaKitRefreshForPullUpCompletedStatusNorMore;
+    self.pullUpStatus = kHSYCocoaKitRefreshForPullUpCompletedStatusClose;
     
     
 //    [[self.hsy_viewModel.subject deliverOn:[RACScheduler mainThreadScheduler]] subscribeNext:^(HSYCocoaKitRACSubscribeNotification *x) {
@@ -234,7 +236,8 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.hsy_viewModel.hsy_datas.count;
+    NSInteger i = self.hsy_viewModel.hsy_datas.count;
+    return i;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -255,42 +258,42 @@
 
 #pragma mark - UIScrollViewDelegate
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    if (!self.toped && (scrollView.contentOffset.y >= (100-35))) {
-        [self update:YES];
-    } else if (self.tableView.tableHeaderView && (scrollView.contentOffset.y <= (100-35))) {
-        self.headerView.hidden = YES;
-    }
-}
-
-- (void)update:(BOOL)hidden
-{
-    if (hidden) {
-        self.headerView.hidden = NO;
-        self.tableView.tableHeaderView = nil;
-        self.tableView.y = self.headerView.bottom;
-        self.tableView.height = IPHONE_HEIGHT - self.tableView.y;
-        [self.tableView setContentOffset:CGPointZero];
-        self.toped = YES;
-    } else {
-        self.tableView.y = self.customNavigationBar.bottom;
-        //滚动到顶部的动作不能真正滚动到contentOffset.y == 0.0f,而是应该预留一点位置，这样才会再执行“- scrollViewDidScroll:”的委托时，进入“else if”的判断中
-        [self.tableView setContentOffset:CGPointMake(0, 0.5f) animated:YES];
-        [CATransaction begin];
-        @weakify(self);
-        [CATransaction setCompletionBlock:^{
-            @strongify(self);
-            self.tableView.height = IPHONE_HEIGHT - self.tableView.y;
-            self.headerView.hidden = YES;
-            self.toped = NO;
-        }];
-        [self.tableView beginUpdates];
-        self.tableView.tableHeaderView = self.tableHeaderView;
-        [self.tableView endUpdates];
-        [CATransaction commit];
-    }
-}
+//- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+//{
+//    if (!self.toped && (scrollView.contentOffset.y >= (100-35))) {
+//        [self update:YES];
+//    } else if (self.tableView.tableHeaderView && (scrollView.contentOffset.y <= (100-35))) {
+//        self.headerView.hidden = YES;
+//    }
+//}
+//
+//- (void)update:(BOOL)hidden
+//{
+//    if (hidden) {
+//        self.headerView.hidden = NO;
+//        self.tableView.tableHeaderView = nil;
+//        self.tableView.y = self.headerView.bottom;
+//        self.tableView.height = IPHONE_HEIGHT - self.tableView.y;
+//        [self.tableView setContentOffset:CGPointZero];
+//        self.toped = YES;
+//    } else {
+//        self.tableView.y = self.customNavigationBar.bottom;
+//        //滚动到顶部的动作不能真正滚动到contentOffset.y == 0.0f,而是应该预留一点位置，这样才会再执行“- scrollViewDidScroll:”的委托时，进入“else if”的判断中
+//        [self.tableView setContentOffset:CGPointMake(0, 0.5f) animated:YES];
+//        [CATransaction begin];
+//        @weakify(self);
+//        [CATransaction setCompletionBlock:^{
+//            @strongify(self);
+//            self.tableView.height = IPHONE_HEIGHT - self.tableView.y;
+//            self.headerView.hidden = YES;
+//            self.toped = NO;
+//        }];
+//        [self.tableView beginUpdates];
+//        self.tableView.tableHeaderView = self.tableHeaderView;
+//        [self.tableView endUpdates];
+//        [CATransaction commit];
+//    }
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
